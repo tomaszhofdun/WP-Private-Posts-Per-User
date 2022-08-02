c1ccd37a32d1e409bea11f73044628024f86bcba
tomaszhofdun <tomaszhofdun@gmail.com>
2022-08-02 22:17:05 +0200

    added translations

--- App/views/template-gardeners-404.php
+++ App/views/template-gardeners-404.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 \get_header(); ?>
 
-<div class="alert alert-danger" role="alert"><?php \_e("You don't have a permission to access this page", 'private-posts-per-user'); ?></div>
+<div class="alert alert-danger" role="alert"><?php _e("You don't have a permission to access this page", 'private-posts-per-user'); ?></div>
 
 <?php \get_footer(); ?>
--- App/views/template-gardeners.php
+++ App/views/template-gardeners.php
@@ -2,27 +2,25 @@
 \get_header();
 
 $user = \wp_get_current_user();
-$display_name = $user->display_name;
-$user_email = $user->user_email;
+$user_firstname = $current_user->user_firstname;
 ?>
 
 <div class="pppu-tabs">
     <ul class="nav nav-tabs">
-        <li role="presentation" class="active"><a href="#home"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Profil</a></li>
-        <li role="presentation"><a href="#fees"><span class="glyphicon glyphicon-usd" aria-hidden="true"></span> Opłaty</a></li>
-        <li role="presentation"><a href="#messages"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> Wiadomości</a></li>
+        <li role="presentation" class="active"><a href="#home"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> <?php _e('Profile', 'private-posts-per-user') ?></a></li>
+        <li role="presentation"><a href="#fees"><span class="glyphicon glyphicon-usd" aria-hidden="true"></span> <?php _e('Fees for electricity', 'private-posts-per-user') ?></a></li>
+        <li role="presentation"><a href="#messages"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> <?php _e('Messages', 'private-posts-per-user') ?></a></li>
     </ul>
 
     <div class="tab-content">
         <div role="tabpanel" class="tab-pane active" id="home">
-            <p>Witaj <?= $display_name; ?></p>
-            <p>Twój email: <?= $user_email; ?></p>
+            <p><?php _e('Hi,', 'private-posts-per-user') ?> <?= $user_firstname; ?></p>
         </div>
         <div role="tabpanel" class="tab-pane bg-danger" id="fees">
-            <?php \the_field('oplaty'); ?>
+            <?php \the_field('fees'); ?>
         </div>
         <div role="tabpanel" class="tab-pane" id="messages">
-            <?php \the_field('wiadomosci'); ?>
+            <?php \the_field('messages'); ?>
         </div>
     </div>
 </div>
